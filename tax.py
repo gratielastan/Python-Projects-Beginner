480ff74cfb46028fe8c246c139a50a67764da2b8
gratielastan <gratiela.marina.stan@gmail.com>
2025-02-23 08:45:27 +0200

    Taxes
    calculate and convert int to float and float to int

---
+++ tax.py
@@ -0,0 +1,10 @@
+amount = 100
+tax = 0.06
+total = amount +  amount * tax
+print(total) #call the print function to output total
+
+amount = int(10.6) #convert a float to an int - chopping off the decimal part
+print(amount)
+
+amount = float(10) # convert an int to a float
+print(amount)
