427f1aafeaafe54ebb436d25ba48d5a24e3c95f0
gratielastan <gratiela.marina.stan@gmail.com>
2025-03-09 20:01:49 +0200

    Add files via upload
    Wheather with conditional statements.

---
+++ weather.py
@@ -0,0 +1,43 @@
+
+#temperature = 50
+#if temperature > 80:
+#    print("It's too hot!")
+#    print("Stay inside!")
+#elif temperature < 60:
+#    print("It's too cold")
+#    print("Stay inside!")
+#else:
+#    print("Enjoy the outdoors")
+#print("Have a nice day")
+
+##################################    
+#temperature = 75
+#if temperature > 80 or temperature < 60:
+#    print("Stay inside!")
+#else:
+#    print("Enjoy the outdoors")
+
+##################################
+#temperature = 75
+#forecast ="rainy" # cloudy or sunny
+#if temperature > 80 and forecast != "rainy":
+#    print("Go outside!")
+#else:
+#    print("Stay inside!")
+
+##################################
+#forecast = "rainy" # cloudy or sunny
+#if not forecast == "rainy":
+#    print("Go outside!")
+#else:
+#    print("Stay inside!")
+
+##################################
+raining = True # you can set boolean variables to either True or False.
+
+if not raining:
+    print("Go outside!")
+else: 
+    print("Stay inside!")
+    
+    
